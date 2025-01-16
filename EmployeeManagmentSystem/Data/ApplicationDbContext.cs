20538400c564c042fd7034079afa284267775432
Kaliprasad-fullstack <palaliku2@gmail.com>
2025-01-16 16:05:33 +0530

    Add project files.

---
+++ EmployeeManagmentSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManegment.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagmentSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeManagmentSystem/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace EmployeeManegment.Models
+{
+    public class Employee : UserActivity
+    {
+        public int Id { get; set; }
+        public string EmpNo { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string phonenumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string FullName => $"{FirstName} {MiddleName} {LastName}";
+
+        public string Country { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string Dipartment { get; set; }
+        public string Designation { get; set; }
+
+
+
+    }
+}
